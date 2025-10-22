fc367de7b0348e61730bcdbe04ad58a0908d21d9
Alcorher <111181871+alcorher@users.noreply.github.com>
2025-10-22 10:24:43 +0200

    Refactor Profile component to accept props for dynamic rendering and improve code reusability

--- src/app/Gallery.js
+++ src/app/Gallery.js
@@ -1,57 +1,53 @@
 import { getImageUrl } from './utils.js';
 
+function Profile({ nombre, imageId, profesion, premios, descubrio }) {
+  return (
+      <section className="profile">
+        <h2>{nombre}</h2>
+        <img
+          className="avatar"
+          src={getImageUrl(imageId)}
+          alt={nombre}
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profesión: </b> 
+            {profesion}
+          </li>
+          <li>
+            <b>Premios: {premios.length} </b> 
+            {premios.join(", ")}
+          </li>
+          <li>
+            <b>Descubrió: </b>
+            {descubrio}
+          </li>
+        </ul>
+      </section>
+  );
+}
+
+
 export default function Gallery() {
   return (
     <div>
       <h1>Científicos Notables</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('szV5sdG')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profesión: </b> 
-            física y química
-          </li>
-          <li>
-            <b>Premios: 4 </b> 
-            (Premio Nobel de Física, Premio Nobel de Química, Medalla Davy, Medalla Matteucci)
-          </li>
-          <li>
-            <b>Descubrió: </b>
-            polonio (elemento químico)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('YfeOqp2')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profesión: </b> 
-            geoquímica
-          </li>
-          <li>
-            <b>Premios: 2 </b> 
-            (Premio Miyake de geoquímica, Premio Tanaka)
-          </li>
-          <li>
-            <b>Descubrió: </b>
-            un método para medir el dióxido de carbono en el agua de mar
-          </li>
-        </ul>
-      </section>
+      <Profile
+        nombre="Maria Skłodowska-Curie"
+        imageId="szV5sdG"
+        profesion="física y química"
+        premios={["Premio Nobel de Física", "Premio Nobel de Química", "Medalla Davy", "Medalla Matteucci"]}
+        descubrio="polonio (elemento químico)"
+      />
+      <Profile
+        nombre="Katsuko Saruhashi"
+        imageId="YfeOqp2"
+        profesion="geoquímica"
+        premios={["Premio Miyake de geoquímica", "Premio Tanaka"]}
+        descubrio="un método para medir el dióxido de carbono en el agua de mar"
+      />
     </div>
   );
 }

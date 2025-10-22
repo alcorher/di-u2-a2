db7bc645e9f9acd5d56c637c8525cfba3094ca3d
Alcorher <111181871+alcorher@users.noreply.github.com>
2025-10-22 10:11:23 +0200

    Update Avatar component to conditionally set image size and adjust Profile size prop (Ejercicio 2)

--- src/app/Profile.js
+++ src/app/Profile.js
@@ -4,10 +4,8 @@ function Avatar({ person, size }) {
   return (
     <img
       className="avatar"
-      src={getImageUrl(person, 'b')}
+      src={getImageUrl(person, size >= 90 ? 'b' : 's')}
       alt={person.name}
-      width={size}
-      height={size}
     />
   );
 }
@@ -15,7 +13,7 @@ function Avatar({ person, size }) {
 export default function Profile() {
   return (
     <Avatar
-      size={40}
+      size={90}
       person={{ 
         name: 'Gregorio Y. Zara', 
         imageId: '7vQD0fP'
